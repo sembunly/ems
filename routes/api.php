11912a67ab8ef19fb0512ce19bc063850e8d2f71
bunlysem <sembunly2005@gmail.com>
2026-05-13 18:00:47 +0700

    Update API Route and comtroller

--- routes/api.php
+++ routes/api.php
@@ -2,25 +2,25 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\Api\ProductApiController;
-// use App\Http\Controllers\Api\CategoryApiController;
-// use App\Http\Controllers\Api\OrderItemApiController;
-// use App\Http\Controllers\Api\OrderApiController;
-// use App\Http\Controllers\Api\UserApiController;
+use App\Http\Controllers\Api\v1\ProductApiController;
+use App\Http\Controllers\Api\v1\CategoryApiController;
+use App\Http\Controllers\Api\v1\OrderItemApiController;
+use App\Http\Controllers\Api\v1\OrderApiController;
+use App\Http\Controllers\Api\v1\UserApiController;
 
 Route::get('/sales-data', [DashboardController::class, 'salesData']);
 
 // API routes for products
-// Route::apiResource('products', ProductApiController::class);
+Route::apiResource('api-products', ProductApiController::class);
 
 // API routes for categories
-// Route::apiResource('categories', CategoryApiController::class);
+Route::apiResource('api-categories', CategoryApiController::class);
 
 // API routes for order items
-// Route::apiResource('order-items', OrderItemApiController::class);
+Route::apiResource('api-orderitems', OrderItemApiController::class);
 
 // API routes for orders
-// Route::apiResource('orders', OrderApiController::class);
+Route::apiResource('api-orders', OrderApiController::class);
 
 // API routes for users
-// Route::apiResource('users', UserApiController::class);
+Route::apiResource('api-users', UserApiController::class);
